d1a5fe5791afdcc386e90aeea73470b16cc7b3a6
TK Ivie <tkivie@gmail.com>
2017-01-19 19:57:20 -0600

    01/19/2017

--- 06_fizzBuzz/main.go
+++ 06_fizzBuzz/main.go
@@ -5,11 +5,15 @@ import "fmt"
 func main() {
 
 	for i := 1; i <= 100; i++ {
-		if i % 2 == 1 {
-			fmt.Println(i," - Odd")
+		if i%5 == 0 && i%3 == 0 {
+			fmt.Println(i, " - FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println(i, " - Fizz")
+		} else if i%5 == 0 {
+			fmt.Println(i, " - Buzz")
 		} else {
-			fmt.Println(i," - Even")
+			fmt.Println(i)
 		}
-	}
 
+	}
 }
--- 07_sumOfMultiples/main.go
+++ 07_sumOfMultiples/main.go
@@ -4,13 +4,11 @@ import "fmt"
 
 func main() {
 
-	for i := 1; i <= 100; i++ {
-		if i%5 == 0 && i%3 == 0 {
-			fmt.Println(i, " - FizzBuzz")
-		} else if i%3 == 0 {
-			fmt.Println(i, " - Fizz")
-		} else if i%5 == 0 {
-			fmt.Println(i, " - Buzz")
+	sum := 0
+	for i := 1; i < 1000; i++ {
+		if i%5 == 0 || i%3 == 0 {
+			sum += i
+			fmt.Println(i, sum)
 		} else {
 			fmt.Println(i)
 		}
